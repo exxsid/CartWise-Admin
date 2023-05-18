1c5034a954f6a79dd909490ae5f114615a3712ba
exxsid <cortezleoanthony5@gmail.com>
2023-05-18 09:49:10 +0800

    edit

--- signupp.php
+++ signupp.php
@@ -13,9 +13,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -29,60 +27,54 @@
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
-               
+
                 <div class="card position-center">
-                           <img src="img\logo.svg" alt="CartWise Logo" class="rounded mx-auto d-block" alt="..." alt="Lights" style="width: 350px;">
-                        </a>
-                        </div>
-                    <div class="col-lg-12">
-                        <div class="p-5">
+                    <img src="img\logo.svg" alt="CartWise Logo" class="rounded mx-auto d-block" alt="..." alt="Lights" style="width: 350px;">
+                    </a>
+                </div>
+                <div class="col-lg-12">
+                    <div class="p-5">
                         <div class="text-left">
-                                        <h1 class="h4 text-gray-900 mb-4">Sign Up Now</h1>
-                                        <h2 class="h6 text-gray-900 mb-4">Please login to continue</h2>
-                                    </div>
-                            <form class="user">
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                            placeholder="First Name">
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                            placeholder="Last Name">
-                                    </div>
+                            <h1 class="h4 text-gray-900 mb-4">Sign Up Now</h1>
+                            <h2 class="h6 text-gray-900 mb-4">Please login to continue</h2>
+                        </div>
+                        <form class="user">
+                            <div class="form-group row">
+                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
                                 </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                        placeholder="Email Address">
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
                                 </div>
-                                <div class="form-group">
-                                    <input type="username" class="form-control form-control-user" id="exampleInputUsername"
-                                        placeholder="Username">
-                                </div>
-                                <div class="form-group">
-                                    <input type="phone number" class="form-control form-control-user" id="exampleInputPhonenumber"
-                                        placeholder="Phone Number">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword"
-                                        placeholder="Password">
-                                </div>
-                                <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                    Sign In
-                                </a>
-                            </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="forgot-password.html">Already User? Sign In</a>
                             </div>
-                            <div class="text-center">
-                                <a class="small" href="login.html">You own grocery store? Register here</a>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
                             </div>
+                            <div class="form-group">
+                                <input type="username" class="form-control form-control-user" id="exampleInputUsername" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="phone number" class="form-control form-control-user" id="exampleInputPhonenumber" placeholder="Phone Number">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                            </div>
+                            <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                Sign In
+                            </a>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="small" href="forgot-password.html">Already User? Sign In</a>
+                        </div>
+                        <div class="text-center">
+                            <a class="small" href="login.php">You own grocery store? Register here</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
     </div>
 
